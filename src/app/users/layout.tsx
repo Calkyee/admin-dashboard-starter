dfc4f3a72893a353001f9f9181d106059a336a33
Callum <babaocallum@gmail.com>
2025-08-17 13:19:00 +0800

    chore(layout): added in proper grid styling with defined height for the grid

--- src/app/users/layout.tsx
+++ src/app/users/layout.tsx
@@ -11,7 +11,7 @@ const layout = ({children}: Props) => {
      flex-1 flex flex-col 
     '>
       <TopNav />  
-      <div>
+      <div className="flex-1 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 auto-rows-[110px] gap-4 p-4">
         {children}
       </div>
     </div>
