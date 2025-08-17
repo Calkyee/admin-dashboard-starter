6e0f30058399c4044c04029f696663a1df1d7130
Callum <babaocallum@gmail.com>
2025-08-17 18:41:42 +0800

    feat(login): added in login form styles

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { signIn } from "next-auth/react";
 import { useState } from "react";
 
@@ -25,26 +24,40 @@ export default function LoginPage() {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Sign In</button>
-      </form>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-    </div>
+    <div className="flex-1 flex justify-center items-center">
+      <div className="w-6/12 h-8/12 bg-white rounded-lg shadow-lg flex justify-center items-center ">
+        <form onSubmit={handleLogin} className="h-8/12 w-6/12 flex flex-col justify-around">
+          <div className="flex flex-col">
+            <label>Email</label>
+            <input type="text"
+            onChange={(e)=>setEmail(e.target.value)} 
+            className="px-4 py-2 border 
+            border-gray-300 rounded-md 
+            focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+            />
+          </div>
+          <div className="flex flex-col">
+            <label>Password</label>
+            <input type="password"
+            onChange={(e)=>setPassword(e.target.value)} 
+            className="px-4 py-2 border 
+            border-gray-300 rounded-md 
+            focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+            />
+          </div>
+            <div className="flex justify-center w-full">
+              <button 
+              type="submit"
+              className="w-8/12 h-fit bg-[#38404F] 
+              text-white font-bold text-2xl p-2 rounded-lg 
+              hover:bg-[#334F82] hover:cursor-pointer duration-100 ease-in-out
+              ">LOGIN</button>
+            </div>
+
+        </form>
+      </div>
+    </div>   
   );
 }
