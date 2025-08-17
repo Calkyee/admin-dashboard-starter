cc199c82be7f9b89bd7027a57b8354dc1607422d
Callum <babaocallum@gmail.com>
2025-08-17 12:17:25 +0800

    feat(SideNav): basic tailwindcss backgruond styles

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const SideNav = () => {
   return (
-    <nav>
+    <nav className='
+    w-60 md:w-64 lg:w-72 
+    min-h-full
+    bg-[#38404F]
+    text-white
+    '>
 
-      
+
     </nav>
   )
 }
