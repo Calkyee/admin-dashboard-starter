bb4855f1e17872f7e7bb0e5d05cbd45f5aee90e8
Callum <babaocallum@gmail.com>
2025-08-17 17:52:07 +0800

    feat(api): added in github login

---
+++ src/app/api/auth/[...nextauth/route.ts
@@ -0,0 +1,45 @@
+import NextAuth from "next-auth"; 
+import CredentialsProvider from "next-auth/providers/credentials"; 
+import {PrismaClient} from '@prisma/client'
+import { compare } from 'bcrypt'; 
+
+const prisma = new PrismaClient(); 
+
+export const authOptions = { 
+  providers: [ 
+    CredentialsProvider({
+      name: "Credentials", 
+      credentials: { 
+        email: {label: 'Email', type: 'text'}, 
+        password: {label: 'Password', type: 'password'}
+      },
+      async authorize(credentials){ 
+        if(!credentials?.email || !credentials.password){ 
+          throw new Error("Missing Email or Password")
+        }
+        const user = await prisma.user.findUnique({
+          where: {email: credentials.email},
+        }); 
+        if(!user){ 
+          throw new Error("No user found"); 
+        }
+        const account = await prisma.account.findFirst({
+          where: { userId: user.id, provider: "credentials"}, 
+        }); 
+        if(!account || !account.access_token){ 
+          throw new Error("No credientals or password found"); 
+        }
+        const isValid = await compare(credentials.password, account.access_token); 
+        if(!isValid){ 
+          throw new Error('Invalid password'); 
+        }; 
+        return { 
+          id: user.id, 
+          name: user.name, 
+          email: user.email, 
+          role: user.role
+        }
+      }
+    })
+  ]
+}
