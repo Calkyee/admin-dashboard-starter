eed3d94f698fe05f9197f654de10c0762f73e33e
Callum <babaocallum@gmail.com>
2025-08-17 12:08:14 +0800

    feat(layout): implemented SideNav component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 // import components here 
-import SideNav from '@/app/components/SideNav';
+import SideNav from '@/components/SideNav';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
