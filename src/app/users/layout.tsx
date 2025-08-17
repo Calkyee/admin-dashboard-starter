34df569c9fc02038bcfc1affd25c2e51716f3be7
Callum <babaocallum@gmail.com>
2025-08-17 12:57:42 +0800

    feat(users): Added in custom layout for users

---
+++ src/app/users/layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import TopNav from '@/components/TopNav'; 
+
+interface Props { 
+  children: React.ReactNode
+}
+
+const layout = ({children}: Props) => {
+  return (
+    <div className='
+     flex-1 flex flex-col 
+    '>
+      <TopNav />  
+      {children}
+    </div>
+  )
+}
+
+export default layout
