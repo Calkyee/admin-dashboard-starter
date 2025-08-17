9a073d7e12bcc5660fcaf3a42eb45bdfa5380757
Callum <babaocallum@gmail.com>
2025-08-17 13:19:48 +0800

    feat(page): Added in boilerplate cards

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='
-    flex-1 flex flex-col 
-    
-    '>
-
-
-
-    </div>
+    <>
+    <div className="bg-white p-4 rounded col-span-1 row-span-2">Card 1</div>
+    <div className="bg-white p-4 rounded col-span-1 row-span-2">Card 2</div>
+    <div className="bg-white p-4 rounded col-span-1 row-span-2">Card 3</div>
+    <div className="bg-white p-4 rounded col-span-1 row-span-2">Card 4</div>
+    <div className="bg-white p-4 rounded col-span-2 row-span-2">Card 5</div>
+    <div className="bg-white p-4 rounded col-span-2 row-span-4">Card 6</div>
+    <div className="bg-white p-4 rounded col-span-2 row-span-2">Card 6</div>
+  </>
   )
 }
 
