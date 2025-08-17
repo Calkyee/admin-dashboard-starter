9a26e24c42aafb21b190d5e97133d395deee84ff
Callum <babaocallum@gmail.com>
2025-08-17 13:01:54 +0800

    feat(TopNav): Added in tailwindcss styles

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const TopNav = () => {
   return (
     <div className='
-    
-    
+      h-[60px] md:h-[74px] lg:h-[81px] bg-white
+      shadow-lg
     '></div>
   )
 }
