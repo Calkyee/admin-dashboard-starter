58b7dd4c1313e10c76a0822c8305044585aac7d0
Callum <babaocallum@gmail.com>
2025-08-17 13:20:38 +0800

    feat(page): Added in shadows to each card

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 const page = () => {
   return (
     <>
-    <div className="bg-white p-4 rounded col-span-1 row-span-2">Card 1</div>
-    <div className="bg-white p-4 rounded col-span-1 row-span-2">Card 2</div>
-    <div className="bg-white p-4 rounded col-span-1 row-span-2">Card 3</div>
-    <div className="bg-white p-4 rounded col-span-1 row-span-2">Card 4</div>
-    <div className="bg-white p-4 rounded col-span-2 row-span-2">Card 5</div>
-    <div className="bg-white p-4 rounded col-span-2 row-span-4">Card 6</div>
-    <div className="bg-white p-4 rounded col-span-2 row-span-2">Card 6</div>
+    <div className="bg-white shadow-lg p-4 rounded col-span-1 row-span-2">Card 1</div>
+    <div className="bg-white shadow-lg p-4 rounded col-span-1 row-span-2">Card 2</div>
+    <div className="bg-white shadow-lg p-4 rounded col-span-1 row-span-2">Card 3</div>
+    <div className="bg-white shadow-lg p-4 rounded col-span-1 row-span-2">Card 4</div>
+    <div className="bg-white shadow-lg p-4 rounded col-span-2 row-span-2">Card 5</div>
+    <div className="bg-white shadow-lg p-4 rounded col-span-2 row-span-4">Card 6</div>
+    <div className="bg-white shadow-lg p-4 rounded col-span-2 row-span-2">Card 6</div>
   </>
   )
 }
