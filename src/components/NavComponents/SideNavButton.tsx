69a5dabd4c220697cb0e8bb0a32aed7b4129e52c
Callum <babaocallum@gmail.com>
2025-08-18 10:26:56 +0800

    feat(Nav): Updated Css styling to disable navigation while the user is not authenticated

--- src/components/NavComponents/SideNavButton.tsx
+++ src/components/NavComponents/SideNavButton.tsx
@@ -1,30 +1,38 @@
-// components/Nav/SideNavLink.tsx
-"use client";
-
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useSession } from 'next-auth/react';
+
 
 type Props = {
   href: string;
   children: React.ReactNode;
   exact?: boolean;
+  isLoading:  boolean; 
 };
 
-export default function SideNavLink({ href, children, exact = false }: Props) {
+export default function SideNavLink({ href, children, exact = false , isLoading}: Props) {
+  const {data, status} = useSession(); 
+    
   const pathname = usePathname();
   const isActive = exact ? pathname === href : pathname.startsWith(href);
+  const onclick = ()=>{ 
+    console.log('[SESSION STATUS]: ', status); 
+  }
+
 
   return (
     <Link
-      href={href}
+      onClick={onclick}
+      href={isLoading ? '/login' : href}
       aria-current={isActive ? "page" : undefined}
       className={`
-        text-white font-medium text-[24px] p-2 rounded-md
-        hover:bg-white/10 focus:outline-none focus:ring-2 focus:ring-white/20
+        ${isLoading ? `cursor-not-allowed`: ``}
+      text-white font-medium text-[24px] p-2 rounded-md
+      hover:bg-white/10 focus:outline-none focus:ring-2 focus:ring-white/20
         w-10/12 text-center
-        ${isActive ? "bg-white/15 font-medium" : "text-white"}
+        ${isActive ? "bg-white/15 font-medium" : "text-white"}        
       `}
-    >
+      >
       {children}
     </Link>
   );
--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,24 +1,28 @@
+"use client";
 import React from 'react'
+import { useSession } from 'next-auth/react';
 import SideNavButton from '@/components/NavComponents/SideNavButton'; 
 
+
 const SideNav = () => {
+  const {data, status} = useSession(); 
+  const isloading = true; 
   return (
     <nav className='
     w-60 md:w-64 lg:w-72 
     min-h-full
     bg-[#38404F]
     text-white
-
     flex flex-col  
     items-center justify-around
     '>
       <h2 className='
       font-bold text-[24px]
       '>app_name/Admin</h2>
-      <SideNavButton href='/users'>
+      <SideNavButton href='/users' isLoading={isloading}>
         Users
       </SideNavButton>
-      <SideNavButton href='/admins'>
+      <SideNavButton href='/admins' isLoading={isloading}>
         Administrators
       </SideNavButton>
 
