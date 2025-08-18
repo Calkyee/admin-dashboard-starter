1be5027939df0348ab5084a50d1dcd6eb75381ad
Callum <babaocallum@gmail.com>
2025-08-18 10:42:34 +0800

    feat(auth): added secure routing to the auth group

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,31 @@
-"use client";
+'use client'; 
+import React, { useEffect } from 'react'
+import { useSession } from 'next-auth/react';
+import { useRouter, usePathname } from 'next/navigation';
 
-import { SessionProvider } from "next-auth/react";
 
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+interface Props { 
+  children: React.ReactNode;
 }
+
+const layout = ({children}: Props) => {
+  const router = useRouter();
+  const params = usePathname();  
+  const {data, status} = useSession();   
+  
+  useEffect(()=>{ 
+    console.log('[USER  DATA]: ', data); 
+    if(status === 'unauthenticated'){ 
+      router.push('/login'); 
+    }
+    if(params === '/login' && status === 'authenticated'){ 
+      router.push('/'); 
+    }
+  }, [status, router]); 
+  
+  return (
+    <>{children}</>
+  )
+}
+
+export default layout
