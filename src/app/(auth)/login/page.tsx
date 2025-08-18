ec2a8a3ba8aeeab4c4cef1974814389eb4fed900
Callum <babaocallum@gmail.com>
2025-08-18 14:51:08 +0800

    feat(login): setup imports for the UserSchema to setup input verification later

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,6 +2,7 @@
 import { signIn } from "next-auth/react";
 import { useState } from "react";
 
+
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
