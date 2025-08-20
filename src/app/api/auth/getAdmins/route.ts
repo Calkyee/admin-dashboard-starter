47da8c2ef2592ed4f2a490e36796d8c860b102d2
Callum <babaocallum@gmail.com>
2025-08-20 12:40:57 +0800

    feat(admins): added in a getAdmins route to get all admins

--- src/app/api/auth/getAdmins/route.ts
+++ src/app/api/auth/getAdmins/route.ts
@@ -5,12 +5,22 @@ import {hash} from 'bcrypt';
 import {ZodError, z} from 'zod'; 
 import { UserSchema } from "@/zod"; 
 
-
+// This seems way to easy 😭
 export async function GET(request: NextRequest){ 
   try{
+    const admins = await prisma.user.findMany({ 
+      where: {role: 'ADMIN'}
+    }); 
+
+    if(!admins) return NextResponse.json({
+      error: "No admins found"
+    }, {status: 404}); 
 
 
-
+    return NextResponse.json({ 
+      message: "Successfully found admins", 
+      admins
+    }, {status: 200}); 
   }catch(error){ 
     if(error instanceof ZodError){ 
       return NextResponse.json({
