9adbf36da937394285c8fd3c0d5c346d4d3b6275
Callum <babaocallum@gmail.com>
2025-08-23 18:33:18 +0800

    feat(docker): added hot refresh for docker

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  watchOptions : { 
+    pollIntervalMs: 500
+  }
 };
 
 export default nextConfig;
