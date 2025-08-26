b52a2d0fe84fbbe7bd09fd9f9b5251d4c9c7d24e
Callum <babaocallum@gmail.com>
2025-08-26 14:57:27 +0800

    feat(AdminCardForm): Seperated the form to a different file for modularity

---
+++ src/components/Form/AdminCardForm.tsx
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import { UserSchema } from '@/zod';
+import { z } from 'zod';
+
+type userType = z.infer<typeof UserSchema>;
+
+
+const AdminForm = ({ admin }: { admin: userType }) => {
+  const [editingField, setEditingField] = useState<{
+    adminId: string;
+    field: 'name' | 'email' | 'password';
+  } | null>(null);
+
+  const LabelClassNames = `ml-4`;
+  const h2ClassNames = `border border-black rounded-2xl p-2 font-bold whitespace-normal break-words`;
+  const divClassNames = `flex flex-col w-full`;
+
+  return (
+    <form>
+      <div>
+        <label className={LabelClassNames}>Id</label>
+        <h2 className={h2ClassNames}>{admin.id}</h2>
+      </div>
+      {/* Email Field */}
+      <div>
+        {editingField?.adminId === admin.id && editingField.field === 'email' ? (
+          <div className={divClassNames}>
+            <label className={LabelClassNames}>Email</label>
+            <input
+              type="text"
+              className={h2ClassNames}
+              defaultValue={admin.email}
+              onBlur={() => setEditingField(null)}
+            />
+          </div>
+        ) : (
+          <div className={divClassNames}>
+            <label className={LabelClassNames}>Email</label>
+            <h2
+              className={h2ClassNames}
+              onClick={() =>
+                setEditingField({
+                  adminId: admin.id,
+                  field: 'email',
+                })
+              }
+            >
+              {admin.email}
+            </h2>
+          </div>
+        )}
+      </div>
+      {/* Name Field */}
+      <div>
+        {editingField?.adminId === admin.id && editingField.field === 'name' ? (
+          <div className={divClassNames}>
+            <label className={LabelClassNames}>Name</label>
+            <input
+              type="text"
+              className={h2ClassNames}
+              defaultValue={admin.name}
+              onBlur={() => {
+                setEditingField(null);
+              }}
+            />
+          </div>
+        ) : (
+          <div className={divClassNames}>
+            <label className={LabelClassNames}>Name</label>
+            <h2
+              onClick={() =>
+                setEditingField({
+                  adminId: admin.id,
+                  field: 'name',
+                })
+              }
+              className={h2ClassNames}
+            >
+              {admin.name}
+            </h2>
+          </div>
+        )}
+      </div>
+      {/* Password Field */}
+      <div>
+        {editingField?.adminId === admin.id && editingField.field === 'password' ? (
+          <div className={divClassNames}>
+            <label className={LabelClassNames}>Password</label>
+            <input
+              type="text"
+              className={h2ClassNames}
+              defaultValue={admin.passwordHash ?? ''}
+              onBlur={() => setEditingField(null)}
+            />
+          </div>
+        ) : (
+          <div className={divClassNames}>
+            <label className={LabelClassNames}>Password</label>
+            <h2
+              onClick={() => {
+                setEditingField({
+                  adminId: admin.id,
+                  field: 'password',
+                });
+              }}
+              className={h2ClassNames}
+            >
+              {admin.passwordHash ?? ''}
+            </h2>
+          </div>
+        )}
+      </div>
+    </form>
+  );
+};
+
+export default AdminForm; 
