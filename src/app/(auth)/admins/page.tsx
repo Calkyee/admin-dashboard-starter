3c09d7bdf800953b46d0ad5b7de44679fd9e0a85
Callum <babaocallum@gmail.com>
2025-08-26 14:39:37 +0800

    refactored(admins): Refactored Admin Card to make code more modular

--- src/app/(auth)/admins/page.tsx
+++ src/app/(auth)/admins/page.tsx
@@ -1,66 +1,15 @@
 'use client'; 
-import React, { useEffect, useState, useTransition } from 'react'
-import Link from 'next/link' 
+import React from 'react'
+ 
 
 import { UserSchema } from '@/zod';
 import { z } from 'zod';
+import AdminCard from '@/components/Cards/AdminCard';
 
 type userType = z.infer<typeof UserSchema>
 
 
 const page = () => {
-  const [admins, setAdmins] = useState<userType[] | null>(null); 
-  const [error, setError] = useState(''); 
-  const [isLoading, setIsLoading] = useState<boolean>(true); 
-  const [expandedAdminId, setExpandedAdminId] = useState<string | null>(null);
-  const [editingField, setEditingField] = useState<{
-    adminId: string; 
-    field: "name" | "email" |"password"; 
-  } | null>(null); 
-
-  useEffect(()=>{ 
-    // Load on page loading 
-    const fetchAdmins = async()=>{ 
-      const res = await fetch('/api/secure/admins/getAdmins', { credentials: 'include'});
-      // const rawText = await res.text(); 
-      // if(rawText){ 
-      //   console.log('[RAW RESPONSE]: ', rawText)
-      // }
-      const data = await res.json(); 
-
-       
-      console.log('[FETCH ADMINS ERROR]: ', data); 
-      if(!res.ok){ 
-        setIsLoading(false) 
-        setError('Unable to retrieve admins'); 
-        return; 
-      }
-      const admins: userType[] = data.admins; 
-      setIsLoading(false); 
-      setError(''); 
-      setAdmins(admins); 
-      console.log('[ADMINS]: ', admins);
-    }
-    fetchAdmins(); 
-  }, [])  
-
-  const ButtonClassNames = 
-  `
-    flex justify-center
-    w-4/12 h-fit bg-[#2B73F9] 
-    text-white font-bold text-2xl p-2 rounded-sm
-    hover:bg-[#1E4EA9] hover:cursor-pointer duration-100 ease-in-out
-  `
-  const LabelClassNames = `
-    ml-4
-  `
-
-  const h2ClassNames = `
-    border border-black rounded-2xl p-2
-    font-bold
-    whitespace-normal break-words
-  `
-
   return (
     <>
     <div className="bg-white shadow-lg p-4 rounded col-span-1 row-span-2 ">Card 1</div>
@@ -75,151 +24,7 @@ const page = () => {
     </div>
     <div className="bg-white shadow-lg p-4 rounded col-span-2 row-span-2">Card 5</div>
     <div className="bg-white shadow-lg p-4 rounded col-span-2 row-span-4 flex flex-col gap-5">
-      <div className='max-h-fit min-w-full flex flex-row gap-2 text-center'>
-        <div className={ButtonClassNames}>
-        <Link href='/create/admins'>Create Admin</Link>
-      </div>
-      <div className={ButtonClassNames}>
-        <Link href='/create/admins'>Read Admin</Link>
-      </div>
-      <div className={ButtonClassNames}>
-        <Link href='/create/admins'>Update Admin</Link>
-      </div>
-      <div className={ButtonClassNames}>
-        <Link href='/create/admins'>Delete Admin</Link>
-      </div>
-      </div>
-      <div className='flex flex-1 gap-4 flex-col overflow-y-auto max-h-[400px]'>
-        <h2>Admins</h2>
-        {isLoading && <p>Loading admins..</p>}
-        {error && <p className='text-red-500'>{error}</p>}
-        {admins && admins.length > 0 && ( 
-          <ul>
-            {
-              admins.map((admin: userType)=> {
-                if(!admin) return; 
-                return ( 
-                  <div 
-                    key={admin.id}
-                    className={`
-                    p-2
-                    rounded-md 
-                    ${expandedAdminId === admin.id ? 
-                      `bg-gray-300` : 'hover:bg-gray-300'
-                    } 
-                    `}>
-                      <div
-                       onClick={()=>
-                        setExpandedAdminId(expandedAdminId === admin.id ? null : admin.id)
-                      } 
-                      className='w-full h-fit flex flex-row justify-between'>
-                        <h2 className='
-                          font-bold
-                        '>{admin.email}</h2>
-                        <h2>▼</h2>
-                      </div>
-                      <div>
-                        { expandedAdminId === admin.id && ( 
-                          <form>
-                            <div>
-                              <label className={LabelClassNames}>Id</label>
-                              <h2 className={h2ClassNames}>{admin.id}</h2>
-                            </div>
-                            <div>
-                              { editingField?.adminId === admin.id && editingField.field === 'email' ? ( 
-                                <div className='flex flex-col w-full'>
-                                  <label className={LabelClassNames}>Email</label>
-                                  <input 
-                                    type="text" 
-                                    className={h2ClassNames}
-                                    defaultValue={admin.email}
-                                    onBlur={()=>setEditingField(null)}
-                                  />
-                                </div>
-                              )
-                              : ( 
-                                <>
-                                  <label className={LabelClassNames}>Email</label>
-                                  <h2 
-                                    className={h2ClassNames}
-                                    onClick={()=> 
-                                    setEditingField({ 
-                                      adminId: admin.id, 
-                                      field: 'email'
-                                    })
-                                  }
-                                  >
-                                    {admin.email}
-                                  </h2>
-                                </>
-                              )
-                              }
-                            </div>
-                            <div>
-                              { editingField?.adminId === admin.id && editingField.field === 'name' ? ( 
-                                <div className='flex flex-col w-full'>
-                                  <label className={LabelClassNames}>Name</label>
-                                  <input 
-                                      type="text" 
-                                      className={h2ClassNames}
-                                      defaultValue={admin.name}
-                                      onBlur={()=>setEditingField(null)}
-                                  />
-                                </div>
-                              )
-                              : ( 
-                                <div className='flex flex-col w-full'>
-                                  <label className={LabelClassNames}>Name</label>
-                                  <h2 
-                                    onClick={()=> 
-                                      setEditingField({ 
-                                        adminId: admin.id, 
-                                        field: 'name'
-                                      })
-                                    }
-                                    className={h2ClassNames}>{admin.name}</h2>
-                                </div>
-                              )
-
-                              }
-                            </div>
-                            <div>
-                              { editingField?.adminId === admin.id && editingField.field === 'password' ? ( 
-                                <div className='flex flex-col w-full'>
-                                  <label className={LabelClassNames}>Password</label>
-                                  <input type="text" 
-                                    className={h2ClassNames}
-                                    defaultValue={admin.passwordHash ?? ""}
-                                    onBlur={()=>setEditingField(null)}
-                                  />
-                                </div>
-                              )
-                              : ( 
-                                <div className='flex flex-col w-full'>
-                                  <label className={LabelClassNames}>Password</label>
-                                  <h2
-                                    onClick={()=> 
-                                      setEditingField({ 
-                                        adminId: admin.id, 
-                                        field: 'password'
-                                      })
-                                    } 
-                                    className={h2ClassNames}>{admin.passwordHash ?? ""}</h2>
-                                </div>
-                              )
-                              }
-                            </div>
-                          </form>
-                          )
-                        }
-                      </div>
-                  </div>
-                )
-              })
-            }
-          </ul>
-        )}
-      </div>
+      <AdminCard />
     </div>
     <div className="bg-white shadow-lg p-4 rounded col-span-2 row-span-2">Card 6</div>
   </>
