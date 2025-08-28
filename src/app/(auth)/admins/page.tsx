b91fafec0f9ed803552e85f7a9c02c0444ab7b70
Callum <babaocallum@gmail.com>
2025-08-28 14:18:45 +0800

    refactor(admins): Opened up the CurrentAdminsCard divider for better code readability

--- src/app/(auth)/admins/page.tsx
+++ src/app/(auth)/admins/page.tsx
@@ -6,8 +6,11 @@ import CurrentAdminsCard from '@/components/Cards/CurrentAdminsCard';
 const Page = () => {
   return (
     <>
-    <div className="bg-white shadow-lg p-4 rounded col-span-1 row-span-2 "><CurrentAdminsCard /></div>
-    <div className="bg-white shadow-lg p-4 rounded col-span-1 row-span-2">Card 2</div>
+    <div className="bg-white shadow-lg p-4 rounded col-span-1 row-span-2 ">
+      <CurrentAdminsCard />
+    </div>
+    <div className="bg-white shadow-lg p-4 rounded col-span-1 row-span-2">
+    </div>
     <div className="bg-white shadow-lg p-4 rounded col-span-1 row-span-2">Card 3</div>
     <div className="
     col-span-1 row-span-2
