39e4940cb7fdae9b0158c681cd4e8868277b9fc0
Callum <babaocallum@gmail.com>
2025-09-01 16:20:10 +0800

    feat(FailedAdminCards): Added in piechart to visualise logged in users vs failed attempts

--- src/components/Cards/FailedAdminCard.tsx
+++ src/components/Cards/FailedAdminCard.tsx
@@ -2,9 +2,23 @@
 import React, { useEffect, useState } from 'react'
 import { FailedLogin, FailedLoginSchema, Session } from '@/zod';
 
+import {
+  ResponsiveContainer, 
+  PieChart, 
+  Pie,
+  Cell, 
+  Tooltip 
+} from 'recharts'; 
+
+type dataPoint = { 
+  name: string; 
+  value: number; 
+}
+
 const FailedAdminCard = () => {
   const [failedLoginAttempts, setFailedLoginAttempts] = useState<FailedLogin[] | null>(null); 
   const [sessions, setSessions] = useState<Session[] | null>(null); 
+  const [chartData, setChartData] = useState<dataPoint[] | null>(null); 
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsloding] = useState(true);  
   useEffect(()=>{
@@ -32,7 +46,12 @@ const FailedAdminCard = () => {
       const sessions: Session[] = data.currentSessions; 
 
       setSessions(sessions);
+      setChartData([
+        {name: "Failed Login Attempts", value: failedLoginAttempts?.length ?? 0}, 
+        {name: "Current Sessions", value: sessions?.length ?? 0}
+      ]);
       setIsloding(false);  
+       
     }
     // Initial fetch 
     getFailedAttempts(); 
@@ -47,15 +66,35 @@ const FailedAdminCard = () => {
 
   return (
     <>
-      <h2>Failed Login Attempts</h2>
+      <h2>Failed Login Attempts <span className='font-bold'> {failedLoginAttempts?.length ?? 0}</span></h2>
       { isLoading && ( 
         <div>Loading...</div>
       )}
       { error && ( 
         <div className='text-red-500'>{error}</div>
       )}
-      { !isLoading && !error && ( 
-        <h2>Failed Login Attempts: <span className='font-bold'> {failedLoginAttempts?.length ?? '0'}</span></h2>
+      { !isLoading && !error &&  chartData && ( 
+        <ResponsiveContainer width="100%" height={200}>
+          <PieChart>
+            <Pie 
+              data={chartData}
+              dataKey="value"
+              nameKey="name"
+              outerRadius={65}
+              innerRadius={35}
+              label
+            >
+              {chartData.map((entry) => (
+                <Cell 
+                  key={entry.name} 
+                  fill={entry.name === "Failed Login Attempts" ? "#f87171" : "#60a5fa"} 
+                />
+              ))}
+            </Pie>
+            <Tooltip />
+          </PieChart>
+        </ResponsiveContainer>
+
       )}
     </>
   )
