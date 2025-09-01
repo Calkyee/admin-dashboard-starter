dda20df29332670ae6ea5e70f0f1bfd807e6c8cf
Callum <babaocallum@gmail.com>
2025-09-01 16:23:19 +0800

    fix(failedLogins): removed unused request paremeter

--- src/app/api/secure/failedLogins/getFailedLogins/route.ts
+++ src/app/api/secure/failedLogins/getFailedLogins/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { prisma } from "@/lib/store/prisma"; 
 
-export async function GET(request: NextRequest){ 
+export async function GET(){ 
   try{
     const failedLogins = await prisma.failedLogin.findMany();
     return NextResponse.json({ 
