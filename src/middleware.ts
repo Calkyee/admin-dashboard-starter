00ee30ab70998c4697d4fc0df3e1a4f8b68caeef
Callum <babaocallum@gmail.com>
2025-09-03 17:26:56 +0800

    feat(middleware): Added custom verification logic for verification token

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,27 @@ import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 import type { NextRequest } from "next/server";
 import { Role } from "@prisma/client";
+import { prisma } from "@/lib/store/prisma"; 
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const token  = await getToken({ req, secret: process.env.NEXTAUTH_SECRET }) as { 
+    id: string; 
+    verificationToken: string; 
+    role: string; 
+  };
+
+
+  const valid = await prisma.verificationToken.findFirst({
+    where: {
+      identifier: token?.id,
+      token: token?.verificationToken,
+      expires: { gt: new Date() },
+    },
+  });
+  
+  if(!valid){ 
+    return NextResponse.redirect(new URL('/login', req.url)); 
+  }
 
   if (req.nextUrl.pathname.startsWith("/api/secure")) {
     if (!token) {
