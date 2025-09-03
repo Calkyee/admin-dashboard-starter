86a6c98887cee0d7c189fbc08fcfcd99ddfe222a
Callum <babaocallum@gmail.com>
2025-09-03 13:11:27 +0800

    feat(SignOutButton): Added Css styling and implemented Children render for custom text

--- src/components/NavComponents/SignOutButton.tsx
+++ src/components/NavComponents/SignOutButton.tsx
@@ -2,11 +2,22 @@
 import React from 'react'
 import { signOut } from 'next-auth/react'; 
 
-const SignOutButton = () => {
-  
+interface Props { 
+  children: React.ReactNode; 
+}
+
+const SignOutButton: React.FC<Props> = ({children}) => {
   return (
-    <button onClick={()=> signOut({ callbackUrl: '/login'})}>
-      Log out
+    <button onClick={()=> signOut({ callbackUrl: '/login'})}
+    className={`
+      text-white font-medium text-[24px] p-2 rounded-md 
+      hover:bg-red-500/30 focus:outline-none focus:ring-2 focus:ring-white/20
+      w-10/12 text-center
+      cursor-pointer
+      duration-100 ease-in-out transition-all
+      `}
+    >
+      {children}
     </button>
   )
 }
