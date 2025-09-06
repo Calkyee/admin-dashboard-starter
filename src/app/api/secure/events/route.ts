9c62b3a2c7e063e1c1464ea91c031685a5118254
Callum <babaocallum@gmail.com>
2025-09-06 16:50:25 +0800

    feat(Events): Added Route to connect client to backend via SSE

---
+++ src/app/api/secure/events/route.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const connections = new Map<string, WritableStreamDefaultWriter>(); 
+
+export async function GET(req: NextRequest){ 
+  const {searchParams } = new URL(req.url); 
+  const userId = searchParams.get('userId'); 
+
+  if(!userId){ 
+    return NextResponse.json({error: "Missing userId"}, {status: 400}); 
+  }
+
+  const stream = new TransformStream(); 
+  const writer = stream.writable.getWriter(); 
+  connections.set(userId, writer); 
+
+  const encoder = new TextEncoder(); 
+  const interval = setInterval(() => {
+      writer.write(encoder.encode("data: ping\n\n")); 
+  }, 15000);
+
+  req.signal.addEventListener("abort", ()=>{ 
+    clearInterval(interval); 
+    connections.delete(userId); 
+    writer.close(); 
+  })
+
+  return new Response(stream.readable, { 
+    headers: { 
+      'Content-Type': "text/event-stream", 
+      'Cache-Control': 'no-cache', 
+      Connection: 'keep-alive', 
+    }
+  })
+}
+
+
+
+export function sendToUser (userId: string, msg: string){ 
+  const writer = connections.get(userId); 
+  if(!writer) return false; 
+  const encoder = new TextEncoder(); 
+  writer.write(encoder.encode(`data: ${msg}\n\n`)); 
+  return true; 
+}
