441c378a85ea3297a0c92c242b8c5d86869a4a71
Callum <babaocallum@gmail.com>
2025-09-06 17:12:48 +0800

    feat(auth): Connected frontend to backend via SSE source

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 'use client'; 
 import React, { useEffect } from 'react'
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import { useRouter, usePathname } from 'next/navigation';
 
 
@@ -19,6 +19,21 @@ const Layout = ({children}: Props) => {
     if(status === 'unauthenticated'){ 
       return router.push('/login'); 
     }
+    if(status === 'authenticated'){ 
+      console.log('[SSE]: Opening connection for user', data.user?.id); 
+      const es = new EventSource(`/api/secure/events?userId=${data.user?.id}`); 
+      es.onopen = ()=>{ 
+        console.log('[SSE]: Connected Successfully')
+      } 
+      es.onmessage = (e)=>{ 
+        console.log('[SEE MESSAGE]: ',  e.data); 
+        if(e.data === 'force-kick'){ 
+          console.log("[SSE]: Received force kick -> signing out"); 
+          signOut(); 
+        }
+      }
+    }
+
   }, [data, status, router]); 
   
   if(status === 'loading'){ 
