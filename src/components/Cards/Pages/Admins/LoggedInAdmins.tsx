ff512ab7fab6135a600de22cad34a2cf3b0b3517
Callum <babaocallum@gmail.com>
2025-09-06 16:22:15 +0800

    feat(LoggedInAdmins): Added import for AnimatePageSwapping

--- src/components/Cards/Pages/Admins/LoggedInAdmins.tsx
+++ src/components/Cards/Pages/Admins/LoggedInAdmins.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import AnimatePageSwapping from '@/lib/AnimatePageSwapping'; 
 import onClickProps from '@/lib/interfaces/onClickProps';
 import { Session, SessionSchema } from "@/zod"; 
 
@@ -7,41 +8,28 @@ interface Props {
 }
 
 const LoggedInAdmins = ({ setOnClick }: Props) => {
-  const [animateIn, setAnimateIn] = useState(false);
   const [isLoading, setIsLoading] = useState(true);  
 
-  useEffect(() => {
-    const timeout = setTimeout(() => setAnimateIn(true), 2); // slight delay triggers transition
-
-    return () => clearTimeout(timeout);
-  }, []);
 
   const ExitPage = () => {
     setOnClick({ Active: false, Card: undefined });
   };
 
   return (
-    <div
-      className={`p-4 col-span-4 row-span-4 rounded shadow-lg bg-white flex flex-col transition-all duration-500 ease-in-out transform ${
-        animateIn ? 'scale-100 opacity-100' : 'scale-95 opacity-0'
-      }`}
-    >
+    <AnimatePageSwapping>
       <button
         className='bg-black w-2/12 text-white px-2 py-1 rounded cursor-pointer'
         onClick={ExitPage}
       >
         Back to Administrators Page
       </button>
-        <div className='
-          w-full h-full grid grid-rows-4 grid-cols-8 pt-4
-        '>
-        <div className='
-          row-span-4 col-span-3
-          '>
-          <MapLoggedInAdmins />    
-        </div>      
+      <div className='w-full h-full grid grid-rows-4 grid-cols-8 pt-4'>
+        <div className='row-span-4 col-span-3'>
+          <MapLoggedInAdmins />
+        </div>
       </div>
-    </div>
+    </AnimatePageSwapping>
+    
   );
 };
 
