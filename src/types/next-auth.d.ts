da02cee7534ffb4bef61dbbe02ef7a482fd0daa7
Callum <babaocallum@gmail.com>
2025-09-06 17:17:09 +0800

    feat(Types): Extended types for sesion, user and jwt token

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,24 @@
+import NextAuth, { DefaultSession, DefaultUser} from "next-auth";
+
+declare module 'next-auth' { 
+  interface Session { 
+    user: { 
+      id: string; 
+      role?:  string; 
+      permissions?: string[] 
+    } & DefaultSession['user']
+  } 
+  interface User extends DefaultUser {
+    id: string; 
+    role?: string; 
+    permissions: string[] 
+  }
+
+  interface jwt { 
+    id: string; 
+    role?: string; 
+    permissions?: string[]; 
+    verificationToken: string; 
+    isINvalid?: boolean; 
+  }
+}
