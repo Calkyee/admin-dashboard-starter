87e59d3563cd4b1cc1542cc7be99722f0496a9ce
Callum <babaocallum@gmail.com>
2025-09-07 19:50:49 +0800

    fix(AnimatePageSwapping): fixed width problem

--- src/lib/AnimatePageSwapping.tsx
+++ src/lib/AnimatePageSwapping.tsx
@@ -14,24 +14,21 @@ const AnimatePageSwapping = ({children}: Props) => {
   }, []); 
 
   return (
-    <div className={`
-        col-span-${animateIn ? '4' : '1'} 
-        row-span-${animateIn ? '4' : '1'} 
-        transition-all duration-900 
-        ease-out 
-        h-full`}>
-      <div className={`
-        h-full bg-white rounded shadow-lg flex flex-col 
-        transform transition-transform duration-700 ease-out 
-        ${animateIn ? "scale-100" : "scale-90"}
-        transition-opacity duration-500 delay-200 
-        ${animateIn ? "opacity-100" : "opacity-0"}
-        p-${animateIn ? "4" : "0"}
-        `}
+    <div
+      className={`
+    transition-all duration-900 ease-out h-full
+    ${animateIn ? "col-span-4 row-span-4" : "col-span-1 row-span-1"}
+  `}
+    >
+      <div
+        className={`
+      h-full bg-white rounded shadow-lg flex flex-col
+      transform transition-transform duration-700 ease-out
+      transition-opacity duration-500 delay-200
+      ${animateIn ? "scale-100 opacity-100 p-4" : "scale-90 opacity-0 p-0"}
+    `}
       >
-        {
-          children
-        }
+        {children}
       </div>
     </div>
   )
