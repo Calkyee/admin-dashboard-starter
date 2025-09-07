c036153124f81672e368a43f69ae7cdae25459c2
Callum <babaocallum@gmail.com>
2025-09-07 14:54:59 +0800

    feat(FailedAdminCard): Added Props to pass setOnClick for page navigation

--- src/components/Cards/FailedAdminCard.tsx
+++ src/components/Cards/FailedAdminCard.tsx
@@ -1,6 +1,7 @@
 'use client'; 
 import React, { useEffect, useState } from 'react'
 import { FailedLogin, Session } from '@/zod';
+import onClickProps from '@/lib/interfaces/onClickProps.ts';
 
 import {
   ResponsiveContainer, 
@@ -15,12 +16,18 @@ type dataPoint = {
   value: number; 
 }
 
-const FailedAdminCard = () => {
+interface Props {
+    setOnClick: React.Dispatch<React.SetStateAction<onClickProps | null>>
+    onClick?: boolean;
+}
+
+const FailedAdminCard = ({setOnClick}: Props) => {
   const [failedLoginAttempts, setFailedLoginAttempts] = useState<FailedLogin[] | null>(null); 
   const [sessions, setSessions] = useState<Session[] | null>(null); 
   const [chartData, setChartData] = useState<dataPoint[] | null>(null); 
   const [error, setError] = useState<string | null>(null);
-  const [isLoading, setIsloding] = useState(true);  
+  const [isLoading, setIsloding] = useState(true);
+  const [hovering, setHovering] = useState(false);
   useEffect(()=>{
     setIsloding(true); 
     const getFailedAttempts = async()=>{ 
@@ -69,9 +76,26 @@ const FailedAdminCard = () => {
       w-full h-full 
       p-4 rounded 
       border border-transparent hover:border-black 
-    '>
-      <h2>Failed Login Attempts <span className='font-bold'> {failedLoginAttempts?.length ?? 0}</span></h2>
-      { isLoading && ( 
+    ' onMouseEnter={()=>setHovering(true)}
+      onMouseLeave={()=>setHovering(false)}
+    >
+      <div className='
+        w-full h-fit
+        relative
+      '>
+        <h2>Failed Login Attempts <span className='font-bold'> {failedLoginAttempts?.length ?? 0}</span></h2>
+        {!isLoading && hovering && (
+          <button className='
+          absolute
+          right-0 top-0
+          bg-black text-white
+          px-2 py-1
+          rounded
+          cursor-pointer
+        ' onClick={()=>setOnClick({Card: 'FailedAdminsLogin', Active: true})}>View more</button>
+        )}
+      </div>
+      { isLoading && (
         <div>Loading...</div>
       )}
       { error && ( 
