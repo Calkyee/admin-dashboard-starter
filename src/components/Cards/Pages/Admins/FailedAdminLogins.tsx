8ac14187920dcf8702e49647ba18c30dc1480410
Callum <babaocallum@gmail.com>
2025-09-07 14:54:17 +0800

    feat(FailedAdminLogin): Added AnimatePageSwapping to animate page swapping

--- src/components/Cards/Pages/Admins/FailedAdminLogins.tsx
+++ src/components/Cards/Pages/Admins/FailedAdminLogins.tsx
@@ -1,13 +1,36 @@
-'use client';
 import React from 'react';
+import AnimatePageSwapping from "@/lib/AnimatePageSwapping";
+import onClickProps from '@/lib/interfaces/onClickProps';
 
+interface Props {
+  setOnClick: React.Dispatch<React.SetStateAction<onClickProps | null>>;
+}
 
-const FailedAdminLogins = ()=>{
-    return (
+const FailedAdminLogins =  ({setOnClick}: Props)=>{
+
+  const ExitPage = ()=>{
+    setOnClick({
+      Card: undefined,
+      Active: false
+    });
+  }
+
+  return (
+      <AnimatePageSwapping>
+        <div>
+          <button onClick={()=>ExitPage()}
+            className='
+            bg-black text-white
+            px-2 py-1
+            rounded
+            cursor-pointer
+            '
+          >Back to Adminsitrators page</button>
+        </div>
         <div>
 
-
         </div>
+      </AnimatePageSwapping>
     )
 }
 
