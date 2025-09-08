0a1b2b3e62ffbf0a83a629592cbbbe5ae8cd1bd9
Callum <babaocallum@gmail.com>
2025-09-08 12:53:56 +0800

    refactor(MapFailedLogins): Added FailedLoginSchema to validate data at once instead of using a for loop. And used length comparesin in setData to check for new data

--- src/lib/UI/MapFailedLogins.tsx
+++ src/lib/UI/MapFailedLogins.tsx
@@ -1,10 +1,16 @@
 'use client';
 import React, {useEffect, useState} from 'react';
+import { z } from 'zod';
 import { FailedLogin, FailedLoginSchema} from '@/zod';
 import getFailedLogins from "@/lib/UI/ServerComponents/GetFailedLogins";
 
+
+
+const FailedLoginArraySchema = z.array(FailedLoginSchema);
+type FailedLoginArrayType = z.infer<typeof FailedLoginArraySchema>;
+
 interface Props {
-  useMockData: boolean;
+  useMockData: FailedLoginArrayType | undefined;
 }
 
 const MapFailedLogins = ({ useMockData }: Props)=>{
@@ -15,18 +21,13 @@ const MapFailedLogins = ({ useMockData }: Props)=>{
     setIsLoading(true);
     const fetchData = async()=>{
       try{
-        const recievedData: FailedLogin[] = await getFailedLogins();
-        for(const f of recievedData){
-          const isValidated = FailedLoginSchema.safeParse(f);
-          if(!isValidated.success){
-            console.error('[VALIDATION ERROR', isValidated.error.flatten());
-            return;
-          }
+        const recievedData: FailedLoginArrayType = await getFailedLogins();
+        const isValidated = FailedLoginArraySchema.safeParse(recievedData);
+        if(!isValidated.success){
+          console.error('[VALIDATION ERROR]: ', isValidated.error.flatten());
+          return;
         }
-
-        if(JSON.stringify(recievedData) !== JSON.stringify(data)){
-          setData(recievedData);
-        }
+       setData(prev => prev.length !== recievedData.length ? recievedData : prev);
       }finally{
         setIsLoading(false);
       }
