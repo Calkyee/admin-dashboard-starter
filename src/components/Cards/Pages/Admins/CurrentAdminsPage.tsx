214fe12eda85cb91046ef338e35744c26e885b5e
Callum <babaocallum@gmail.com>
2025-09-09 13:40:57 +0800

    feat(CurrentAdminsPage): Added CurrentAdminsPage to display current administrators

---
+++ src/components/Cards/Pages/Admins/CurrentAdminsPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AnimatePageSwapping from "@/lib/AnimatePageSwapping.tsx";
+
+const CurrentAdminsPage = ()=>{
+  return (
+    <AnimatePageSwapping>
+
+
+    </AnimatePageSwapping>
+  )
+}
+
+export default CurrentAdminsPage;
