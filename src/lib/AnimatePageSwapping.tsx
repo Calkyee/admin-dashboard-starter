31f4264ef55ed9d83b7250ec9f480a76e40adbf3
Callum <babaocallum@gmail.com>
2025-09-09 19:23:54 +0800

    feat(AnimatePageSwapping): Made page growing animation more smoother and added overflow-hidden tag

--- src/lib/AnimatePageSwapping.tsx
+++ src/lib/AnimatePageSwapping.tsx
@@ -1,10 +1,13 @@
 import React, {useEffect, useState} from 'react'
+import { motion } from 'framer-motion'
 
 type Props =  { 
   children: React.ReactNode
+  position: string;
 }
+const duration: number = 0.75;
 
-const AnimatePageSwapping = ({children}: Props) => {
+const AnimatePageSwapping = ({children, position}: Props) => {
   const [animateIn, setAnimateIn] = useState(false); 
 
   useEffect(()=>{
@@ -14,22 +17,25 @@ const AnimatePageSwapping = ({children}: Props) => {
   }, []); 
 
   return (
-    <div
-      className={`
-    transition-all duration-900 ease-out h-full
-    ${animateIn ? "col-span-4 row-span-4" : "col-span-1 row-span-1"}
-  `}
-    >
-      <div
+    <div className='col-span-4 row-span-4 grid grid-cols-4 grid-rows-4 overflow-hidden'>
+      <motion.div
+        layout
+        transition={{ duration: duration, ease: "easeInOut" }}
         className={`
-      h-full bg-white rounded shadow-lg flex flex-col
-      transform transition-transform duration-700 ease-out
-      transition-opacity duration-500 delay-200
-      ${animateIn ? "scale-100 opacity-100 p-4" : "scale-90 opacity-0 p-0"}
-    `}
+        ${animateIn ? `col-span-4 row-span-4` : `${position} col-span-1 row-span-1`}
+        `
+        }
       >
-        {children}
-      </div>
+        <motion.div
+          layout
+          initial={{scale: 0.75}}
+          animate={{scale: 1}}
+          transition={{duration: duration, ease: 'easeInOut'}}
+          className="h-full bg-white rounded shadow-lg flex flex-col p-4"
+        >
+          {children}
+        </motion.div>
+      </motion.div>
     </div>
   )
 }
