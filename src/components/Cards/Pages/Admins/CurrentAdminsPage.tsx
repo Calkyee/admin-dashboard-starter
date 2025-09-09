01edaa6f363bace12ca5c79b4fb71d2f45c45433
Callum <babaocallum@gmail.com>
2025-09-09 14:18:41 +0800

    feat(CurrentAdminPages): Added button to navigate back to the administrators page

--- src/components/Cards/Pages/Admins/CurrentAdminsPage.tsx
+++ src/components/Cards/Pages/Admins/CurrentAdminsPage.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import AnimatePageSwapping from "@/lib/AnimatePageSwapping.tsx";
+import AnimatePageSwapping from "@/lib/AnimatePageSwapping";
+import setOnClickProps from '@/lib/interfaces/onClickProps';
 
-const CurrentAdminsPage = ()=>{
+interface Props {
+  setOnClick: React.Dispatch<React.SetStateAction<setOnClickProps | null>>;
+}
+
+const CurrentAdminsPage = ({ setOnClick }: Props)=>{
+
+  const ExitPage = ()=>{
+    setOnClick({
+      Card: undefined, Active: false
+    })
+  }
+
   return (
     <AnimatePageSwapping>
+      <div className='w-full h-full flex flex-col gap-2 '>
+        <div className='w-full h-fit min-h-4'>
+          <button className='
+            bg-black text-white py-2 px-1 rounded cursor-pointer
+            '
+            onClick={()=>ExitPage()}>
+            Return to Administrators Page
+          </button>
+        </div>
 
 
+      </div>
     </AnimatePageSwapping>
   )
 }
