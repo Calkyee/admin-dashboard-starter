bc34fcd012535af6063cccbc9529b1d04bef0388
Callum <babaocallum@gmail.com>
2025-09-09 19:56:15 +0800

    refactor(CurrentAdminsCards): Refactored Tsx and Css styling

--- src/components/Cards/CurrentAdminsCard.tsx
+++ src/components/Cards/CurrentAdminsCard.tsx
@@ -47,51 +47,43 @@ const CurrentAdminsCard = ({setOnClick}: Props) => {
 
   return (
     <div className='
-      w-full h-full 
-      rounded p-2
-      flex flex-col justify-center align-middle
-      border border-transparent hover:border-black
-    ' onMouseEnter={()=>setHovering(true)}
-      onMouseLeave={()=>setHovering(false)}
-      >
-      <div className='w-full min-h-4 relative'>
-        <h2>Current Admins <span className='font-bold'> {numberOfUsers}</span></h2>
-        { hovering && (
-          <button
-            className='
-            absolute right-0 top-0
-           bg-black text-white py-2 px-1 rounded cursor-pointer
-          '
-            onClick={()=>setOnClick({
-              Card: 'CurrentAdmins',
-              Active: true
-          })}>View more</button>
-        )}
-      </div>
-
-      {isloading && (<h2 className='text-red-500'>Loading...</h2>)}
-      {!isloading && ( 
-        <>
-          <div className="h-10/12 w-12/12 mt-4 flex align-middle justify-center">
-            <ResponsiveContainer height='100%' width='100%'>
+    w-full h-full
+    p-4 rounded
+    border border-transparent hover:border-black'
+    onMouseEnter={() => setHovering(true)}
+    onMouseLeave={() => setHovering(false)}>
+      {isloading && (<div className='font-bold'>Loading...</div>)}
+      {!isloading && numberOfUsers && (
+        <div className='w-full h-full pb-4 overflow-hidden '>
+          <div className='w-full h-fit relative pb-4'>
+            <h2 className='w-8/12'>Current Existing Administrators: <span className='font-bold'>{numberOfUsers}</span></h2>
+            { hovering && !isloading && (
+              <button className='
+                absolute
+                right-0 top-0
+                bg-black text-white
+                px-2 py-1
+                rounded
+                cursor-pointer
+              '
+              onClick={()=> setOnClick({Card: 'CurrentAdmins', Active: true})}
+              >
+                view more
+              </button>
+            )}
+          </div>
+          { numberOfUsers && (
+            <ResponsiveContainer height="100%" width="100%">
               <LineChart data={data}>
                 <Line
+                  dataKey={'users'}
                   type="stepAfter"
-                  dataKey="users"
-                  stroke="#2563eb"
-                  strokeWidth={2}
-                  dot={false}
-                  isAnimationActive={false}
                 />
-                <Tooltip
-                  formatter={(value) => [`${value} admins`, ""]}
-                  labelFormatter={(label) => `Index ${label}`}
-                />
+                <Tooltip/>
               </LineChart>
             </ResponsiveContainer>
-          </div>
-
-        </>
+          )}
+        </div>
       )}
     </div>
   )
