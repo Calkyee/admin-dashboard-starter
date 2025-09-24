b607a12eef19327475289c255ad080ec9fab9d46
Callum <babaocallum@gmail.com>
2025-09-24 20:01:08 +0800

    feat(lib/auth.ts): Setup CredentialsProvider with nextAuth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,28 @@
 import { NextAuthOptions } from "next-auth"; 
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import Prisma from "@/lib/store/prismaClient"; 
 
+import CredentialsProvider from "next-auth/providers/credentials";
 
-const authOptions: NextAuthOptions = { 
+import getUserFromCredentials from "@/lib/store/getUserFromCredentials"; 
+
+const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(Prisma), 
+  session: {
+    strategy: "database"
+  },
   providers: [ 
-
+    CredentialsProvider({ 
+      name: "Credentials", 
+      credentials: { 
+        username: {label: "Username", type: "text" }, 
+        password: {label: "Password", type: "password"}
+      }, 
+      async authorize(credentials, req){
+        const user = await getUserFromCredentials(credentials); 
+        return null; 
+      }
+    })
   ]
 }
 
