963a0d9580110a81c659997b0f67a57ad94f580b
Callum <babaocallum@gmail.com>
2025-09-25 18:52:32 +0800

    feat(ui/navigationBar): Added Navigation bar with button rendering

---
+++ src/components/ui/navigationBar/NavigationBar.tsx
@@ -0,0 +1,38 @@
+'use client'; 
+import React from 'react'
+import { useRouter } from 'next/router';
+
+import buttonInterface from '@/lib/interfaces/buttonInterface'
+
+
+interface NavigationBarProps { 
+  Buttons: buttonInterface[]
+}
+
+interface handleOnClickNavProps { 
+  Link: String; 
+}
+
+const NavigationBar = ({Buttons}: NavigationBarProps) => {
+  const router = useRouter(); 
+  const handleOnClick = (Link: string)=>{ 
+    router.push(Link); 
+  }
+  
+  return (
+    <nav>
+      { 
+        Buttons.map((button, idx)=>(
+          <button
+          key={idx}
+          onClick={()=>handleOnClick(button.Link)}
+          >
+            {button.buttonTitle}
+          </button>
+        ))
+      }
+    </nav>
+  )
+}
+
+export default NavigationBar
