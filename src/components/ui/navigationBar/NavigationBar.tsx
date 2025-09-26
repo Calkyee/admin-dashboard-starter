45342d8124270fdbc0f9eb4573a3618ad71ef85d
Callum <babaocallum@gmail.com>
2025-09-26 15:58:06 +0800

    feat(ui/navigationBar): Added Css styling to the navigation bar and buttons

--- src/components/ui/navigationBar/NavigationBar.tsx
+++ src/components/ui/navigationBar/NavigationBar.tsx
@@ -18,12 +18,36 @@ const NavigationBar = ({Buttons}: NavigationBarProps) => {
   }
   
   return (
-    <nav>
+    <nav className='
+      w-2/12
+      h-12/12 
+
+    bg-white 
+      p-2  
+
+      flex flex-col gap-[1.5625rem]
+      justify-start
+      items-center
+    '>
       { 
         Buttons.map((button, idx)=>{
           return(          
             <button
-            className='text-black'
+            className='
+            w-10/12
+            h-fit 
+            min-h-12
+            py-2 px-2
+
+            rounded
+            text-center
+          text-white
+            text-[1.5rem]
+            font-bold
+
+            bg-[#9C5CA6]
+
+            '
             key={idx}
             onClick={()=>handleOnClick(button.Link)}
             >
