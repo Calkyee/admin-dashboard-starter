b62cbdeac33c1bf6cad3b3f71a5c35a515902247
Callum <babaocallum@gmail.com>
2025-09-26 18:45:11 +0800

    feat(mainApp/page.tsx): Added BarGraph to first card

--- src/app/(mainApp)/page.tsx
+++ src/app/(mainApp)/page.tsx
@@ -1,11 +1,11 @@
-
 import Card from "@/components/ui/Cards/Card"; 
+import BarGraph from "@/components/ui/Charts/BarGraph";
 
 export default function Home() {
   return (
     <>
       <Card CardCol={1} CardRow={1}>
-
+        <BarGraph />
       </Card>
       <Card CardCol={1} CardRow={1}>
 
