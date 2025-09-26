94a43da09b84fdd7862bd043e47ff869e8119c0a
Callum <babaocallum@gmail.com>
2025-09-26 18:34:36 +0800

    feat(mainApp/layout.tsx): Added 4 by 4 Grid

--- src/app/(mainApp)/layout.tsx
+++ src/app/(mainApp)/layout.tsx
@@ -8,12 +8,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className='
       h-full w-full
-      
+      grid grid-cols-4 grid-rows-4
+      gap-4
     '>
       {children}
     </div>
-      
-    
   )
 }
 
