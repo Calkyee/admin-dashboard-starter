ad97faca38748590f809a5c2dd784d2425bbd67b
Callum <babaocallum@gmail.com>
2025-09-26 18:39:17 +0800

    feat(ui/Cards): Added children to Card

--- src/components/ui/Cards/Card.tsx
+++ src/components/ui/Cards/Card.tsx
@@ -3,17 +3,19 @@ import React from 'react'
 interface CardProps { 
   CardCol: number; 
   CardRow: number; 
+  children?: React.ReactNode; 
 }
 
-const Card = ({CardCol, CardRow}: CardProps) => {
+const Card = ({CardCol, CardRow, children}: CardProps) => {
   return (
     <div className={`
       bg-white shadow rounded
       col-span-${CardCol}
       row-span-${CardRow}
     `}>
-
-
+      {
+        children
+      }
     </div>
   )
 }
