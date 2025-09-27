4a8c94f07e5c5a08753fe0a269435d2cc4707088
Callum <babaocallum@gmail.com>
2025-09-27 14:28:08 +0800

    feat(Charts/BarGraph.tsx): Added BarGraph component with MockData

--- src/components/ui/Charts/BarGraph.tsx
+++ src/components/ui/Charts/BarGraph.tsx
@@ -1,11 +1,46 @@
 'use client'; 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BarChart, Tooltip, ResponsiveContainer, Bar } from 'recharts'; 
+import MockUsers from "@/lib/mockData/MockUsers"; 
 
-const BarGraph = () => {
+interface BarGraphProps { 
+  useMockData?: boolean; 
+  dataSource?: string; 
+}
+
+interface dataPoint { 
+  name: string; 
+  users: number; 
+}
+
+
+
+const BarGraph = ({useMockData, dataSource}: BarGraphProps) => {
+  const [chartData, setChartData] = useState<dataPoint[]>([]); 
+  const [isLoading, setIsLoading] = useState(true); 
+
+  useEffect(()=>{
+    setIsLoading(true); 
+    if(useMockData){ 
+      setChartData(MockUsers);
+      setIsLoading(false); 
+      return; 
+    }
+    // Implement live data source later 
+  }, [useMockData, dataSource]); 
+
   return (
-    <div>
-
-
+    <div className='w-full h-full p-2'>
+      { isLoading ? ( 
+        <h2>Loading...</h2>
+      ) : (
+        <ResponsiveContainer width="100%" height="100%">  
+          <BarChart data={chartData}>
+            <Tooltip itemStyle={{color: "black"}} labelStyle={{color: 'black'}}/>
+            <Bar dataKey="users" fill='transparent' strokeWidth={2} stroke='black'/>
+          </BarChart>
+        </ResponsiveContainer>
+       )}
     </div>
   )
 }
