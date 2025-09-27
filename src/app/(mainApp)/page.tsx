01dc3a003ab95cb4c5f7a767d28febb428ed47a5
Callum <babaocallum@gmail.com>
2025-09-27 14:42:22 +0800

    feat(mainApp/page.tsx): Imported and rendered LineGraph

--- src/app/(mainApp)/page.tsx
+++ src/app/(mainApp)/page.tsx
@@ -1,5 +1,6 @@
 import Card from "@/components/ui/Cards/Card"; 
 import BarGraph from "@/components/ui/Charts/BarGraph";
+import LineGraph from "@/components/ui/Charts/LineGraph";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
         <BarGraph useMockData={true}/>
       </Card>      
       <Card CardCol={1} CardRow={1}>
-        
+        <LineGraph useMockData={true}/>
       </Card>
       <Card CardCol={1} CardRow={1}>
       
