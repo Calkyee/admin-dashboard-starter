492609c576f67134446e20d2c19a2f136ee8947e
Callum <babaocallum@gmail.com>
2025-09-27 15:45:10 +0800

    feat(mainApp/page.tsx): Added PieChart imports and rendering

--- src/app/(mainApp)/page.tsx
+++ src/app/(mainApp)/page.tsx
@@ -1,6 +1,8 @@
 import Card from "@/components/ui/Cards/Card"; 
 import BarGraph from "@/components/ui/Charts/BarGraph";
 import LineGraph from "@/components/ui/Charts/LineGraph";
+import StepGraph from "@/components/ui/Charts/StepGraph";
+import PieChart from "@/components/ui/Charts/PieGraph"; 
 
 export default function Home() {
   return (
@@ -12,10 +14,10 @@ export default function Home() {
         <LineGraph useMockData={true}/>
       </Card>
       <Card CardCol={1} CardRow={1}>
-      
+        <StepGraph useMockData={true}/>
       </Card>
       <Card CardCol={1} CardRow={1}>
-      
+        <PieChart useMockData={true}/>
       </Card>
     </>
   );
