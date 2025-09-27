f01b34b390e24a321bc4e496a6dd9f1581b0e099
Callum <babaocallum@gmail.com>
2025-09-27 14:27:19 +0800

    feat(mockData/MockUsers.ts): Changed from 12 months of data to 6 months

--- src/lib/mockData/MockUsers.ts
+++ src/lib/mockData/MockUsers.ts
@@ -9,13 +9,7 @@ const Users: dataPoint[] = [
   { name: 'Mar', users: 390 },
   { name: 'Apr', users: 500 },
   { name: 'May', users: 610 },
-  { name: 'Jun', users: 450 },
-  { name: 'Jul', users: 700 },
-  { name: 'Aug', users: 640 },
-  { name: 'Sep', users: 720 },
-  { name: 'Oct', users: 680 },
-  { name: 'Nov', users: 750 },
-  { name: 'Dec', users: 800 },
+  { name: 'Jun', users: 450 }
 ];
 
 export default Users; 
