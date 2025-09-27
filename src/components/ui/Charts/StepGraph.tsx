3c558db35a4a321b060f687feeae0952dfe9f9dc
Callum <babaocallum@gmail.com>
2025-09-27 15:17:38 +0800

    feat(Charts/StepGraph.tsx): Added Stepgraph

---
+++ src/components/ui/Charts/StepGraph.tsx
@@ -0,0 +1,48 @@
+'use client'; 
+import React, {useEffect, useState} from 'react'; 
+import { LineChart, Line, Tooltip, ResponsiveContainer } from 'recharts'; 
+import MockUsers from "@/lib/mockData/MockUsers"; 
+
+interface LineGraphProps { 
+  useMockData?: boolean; 
+  dataSource?: string; 
+}
+
+interface dataPoint { 
+  name: string; 
+  users: number; 
+}
+
+const StepGraph = ({useMockData, dataSource}: LineGraphProps)=>{ 
+  const [isLoading, setIsLoading] = useState(true); 
+  const [chartData, setChartData] = useState<dataPoint[]>([]); 
+
+  useEffect(()=>{ 
+    setIsLoading(true); 
+    if(useMockData){ 
+      setChartData(MockUsers);
+      setIsLoading(false);  
+      return; 
+    }
+    // Implement Live data source later 
+  }, [useMockData, dataSource]); 
+
+  return ( 
+    <div className='
+      w-full h-full p-2 
+    '>
+      {isLoading ? ( 
+        <h2>Loading...</h2>
+      ) : ( 
+        <ResponsiveContainer>
+          <LineChart data={chartData}>
+            <Tooltip />
+            <Line dataKey="users" strokeWidth={2} stroke='black' type='stepAfter' dot={false}/>
+          </LineChart>
+        </ResponsiveContainer>
+      )}
+    </div>
+  )
+}
+
+export default StepGraph; 
