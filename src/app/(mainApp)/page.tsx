26a6789439dc54603a37af42677044e45ecf08e8
Callum <babaocallum@gmail.com>
2025-09-29 12:47:20 +0800

    feat(mainApp/page.tsx): Passed Heading Config into Card Render

--- src/app/(mainApp)/page.tsx
+++ src/app/(mainApp)/page.tsx
@@ -2,12 +2,23 @@
 import Card from "@/components/ui/Cards/Card"; 
 import DashBoardConfig from "@/app/(mainApp)/DashBoardConfig";
 
+const GetTitleFromUrl = (title: string)=>{ 
+  const Title = title.replace('/', '');
+  
+  return Title.trim(); 
+}
 
 export default function Home() {
   return (
     <>
       { DashBoardConfig.map((config, idx)=>( 
-        <Card key={idx} CardCol={config.CardCol} CardRow={config.CardRow}>
+        <Card 
+          key={idx} 
+          CardCol={config.CardCol} 
+          CardRow={config.CardRow} 
+          DetailPageUrl={config.DetailPageUrl ? config.DetailPageUrl : undefined} 
+          Title={config.Title ? config.Title : 'TITLE'}
+          >
             {config.Component}
         </Card>
       ))
