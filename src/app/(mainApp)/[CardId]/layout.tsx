34afa54fbe1a0e2819711e497d352656238d4d56
Callum <babaocallum@gmail.com>
2025-09-30 11:26:22 +0800

    feat(CardId): Added Sub-navigation for Buttons

--- src/app/(mainApp)/[CardId]/layout.tsx
+++ src/app/(mainApp)/[CardId]/layout.tsx
@@ -1,17 +1,42 @@
+'use client'; 
 import React from 'react'
+
+import { useParams } from 'next/navigation'; 
+
+import Title from '@/components/Pages/Components/Title'
+import ReturnButton from '@/components/Pages/Components/ReturnButton'
 
 interface LayoutPropps { 
   children: React.ReactNode
 }
 
 const Layout = ({ children }: LayoutPropps) => {
+  const {CardId} = useParams(); 
   return (
     <div className='
       col-span-4 row-span-4 
+            
+      bg-white shadow rounded
     '>
+        <div className='w-full h-fit flex flex-col '>
+          <Title>TITLE</Title>
+        <div className='
+          w-full h-fit flex flex-row 
+          pl-12 gap-2 
+        '>
+          <ReturnButton defaultLink={`/${CardId}`} relativeWidth={false}>
+            Graph
+          </ReturnButton>
+          <ReturnButton defaultLink={`/Actions`} relativeWidth={false}>
+            Actions
+          </ReturnButton>
+          <ReturnButton defaultLink={`/AuditLogs`} relativeWidth={false}>
+            Audit Logs
+          </ReturnButton>
+        </div>
+      </div>
       <div className='
-        w-full  h-full   
-      bg-white shadow rounded 
+        w-full  h-full 
       '>
         {children}
       </div>
